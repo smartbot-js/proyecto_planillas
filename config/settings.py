939f3fa3ea2f84a7c84c6c79a2426e8f1c3afe1b
Jhon Stiven Medina Vargas <jhonstmedinav@gmail.com>
2026-01-15 12:38:12 -0500

    login token

--- config/settings.py
+++ config/settings.py
@@ -99,6 +99,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
